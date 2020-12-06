58d064075945194f041df6469ae02d0386135562
kkm2005 <pak2005@hanmail.net>
2020-12-07 02:14:59 +0900

    SideBar 추가

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,16 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+/* Side Bar */
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('.sidebar');
+var lineOne = document.querySelector('.sidebar .menu-btn .line--1');
+var lineTwo = document.querySelector('.sidebar .menu-btn .line--2');
+var link = document.querySelector('.sidebar .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
